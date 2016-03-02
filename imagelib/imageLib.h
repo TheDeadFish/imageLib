7aa7ecb7223f1b033e497f98af642614cc2a416a
TheDeadFish <michaelstamper1@gmail.com>
2016-03-02 05:02:59 +0000

    1. imageLib.h, minor cleanup

--- imagelib/imageLib.h
+++ imagelib/imageLib.h
@@ -6,25 +6,25 @@
 
 #define ImageLib_assert assert
 
+namespace ImageLib {
+
 NEVER_INLINE
 static __stdcall byte* fBuf(FILE* fp, int size) { fp->_cnt -= size;
 	return (byte*)release(fp->_ptr, fp->_ptr+size); }
 NEVER_INLINE
 static __stdcall int wBuf(FILE* fp) { int result = _flsbuf(0, fp); 
 	fp->_cnt++; fp->_ptr--; return result; }
-static int snapToBits(int val)
-	{	return ((__builtin_clz(val-1)^31)+1); }
 #define byte_reg(x) asm("" : "+q"(x))
 #define byte_move(d,s) asm volatile("hello" : "=r"(d) : "r"(s))
 
+class Image;
+class ImageObj;
 
-
-
-
+}
 
 #include "rect.h"
 #include "color.h"
-#include "image.h"
 #include "imghlp.h"
+#include "image.h"
 #include "resample/resample.h"
 #endif
